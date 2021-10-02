4177b0cdccb49986d40a559b1ad243dfac3c8582
steven4389 <40493520+steven4389@users.noreply.github.com>
2021-10-01 20:49:36 -0500

    componente Gallery ok

--- react/learningreact/src/components/gallery/Gallery.js
+++ react/learningreact/src/components/gallery/Gallery.js
@@ -7,30 +7,43 @@ const Gallery = ({ keyword }) => {
 
     useEffect(() => {
         if (keyword !== "") {
-            let producto = products.find(product => product.nombre === keyword);
-            if (producto !== undefined) {
-                setProductos([producto]);
-            } else {
-                setProductos([]);
-            }
-        }else{
+            console.log('keyword', keyword);
+            let producto = products.find(product => product.nombre.match(keyword));
+
+            //if else con operador ternario
+            producto !== undefined ? setProductos([producto]) : setProductos([]);
+
+        } else {
             setProductos(products);
         }
     }, [keyword])
 
+
+
     return (
         <div className="galleryComponent">
-            {productos.length > 0 &&
+            {(productos !== undefined && productos.length > 0) ?
                 productos.map(item => {
                     return (<div className="itemProduct">
                         <div><strong>Nombre:</strong> {item.nombre}</div>
                         <div><strong>Precio:</strong> {item.precio}</div>
                         <div><strong>Calificación:</strong> {item.calificacion}</div>
                     </div>);
-                })
+                }) :
+                productos !== undefined ?
+                    <div>
+                        Ningun producto coincide con la busqueda
+                    </div>
+                    :
+                    <div>
+                        Error en la conexión, intenta mas tarde
+                    </div>
             }
         </div>
     )
 }
 
 export default Gallery
+
+
+//productos.length > 0 &&
